e0b68192c98c9cd00bbf05c2cc4abe41445f0859
Jeromy Cannon <jeromy@swirldslabs.com>
2024-05-10 17:31:36 +0100

    feat: add k8s-tool-setup.sh
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

---
+++ runner/k8s-tools-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+sudo apt-get update
+sudo apt-get install -y kubectl vim
+kubectl cluster-info
+/bin/bash -c "CI=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/runner/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get -y install build-essential
+brew install derailed/k9s/k9s
+sleep 10
+k9s -A
+kubectl auth can-i patch pod
+kubectl auth can-i update pod
+kubectl auth can-i delete pod
+kubectl auth can-i edit pod
+kubectl auth can-i create pod
+
